8fa8ff21952edba4b1294bbbcb196d70369f7a1e
DEMl4N <delfirmamentos@gmail.com>
2024-03-15 00:09:44 +0900

    Feat: WebView

--- Planner/ContentView.swift
+++ Planner/ContentView.swift
@@ -6,16 +6,48 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    let webView = WebView(url: "https://www.naver.com")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            webView
+                .frame(width: .infinity, height: .infinity)
+            
         }
         .padding()
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    var urlRequest: URLRequest
+    private var webView: WKWebView
+    
+    init(url: String) {
+        self.webView = WKWebView()
+        self.urlRequest = URLRequest(url: URL(string: url)!)
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return webView
+    }
+        
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(urlRequest)
+    }
+        
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    class Coordinator: NSObject {
+        let parent: WebView
+        
+        init(parent: WebView) {
+            self.parent = parent
+        }
     }
 }
 
