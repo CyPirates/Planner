b7df3afccb6739c4af234e738382b5e05f3bfb70
DEMl4N <delfirmamentos@gmail.com>
2024-03-15 00:01:54 +0900

    Initial Commit

---
+++ Planner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planner
+//
+//  Created by Demian on 2024/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Demian on 2024/03/15.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
